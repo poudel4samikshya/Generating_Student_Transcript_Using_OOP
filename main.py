c712bf870d336c0dd9d4b1f152368b31271584ef
Samikshya_poudel <50699869+poudel4samikshya@users.noreply.github.com>
2021-12-03 14:44:14 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,93 @@
+
+#Using OOP concept to print the trancript
+class Demographics:
+    def __init__(self,ID,Age,Gender,In_state,*args,**kwargs):
+        super(Demographics,self).__init__(*args,**kwargs)
+
+        self.ID = ID
+        self.Age = Age
+        self.Gender = Gender
+        self.In_state = In_state
+
+
+        def tution_fees_percredit(self):
+
+            if(self.In_state == "yes" or self.In_state == "Yes"):
+                self.fees = 560
+            else:
+                self.fees = 913
+            return self.fees
+        self.fees = tution_fees_percredit(self)
+
+
+class Grade:
+    def __init__(self,Math_score,Science_score,English_score,*args,**kwargs):
+        super(Grade,self).__init__(*args,**kwargs)
+
+        self.Math_score = Math_score
+        self.Science_score = Science_score
+        self.English_score = English_score
+
+
+        self.average = ((self.Math_score + self.Science_score + self.English_score)/3)
+
+        def grade_calc(self) :
+            if(self.average > 90 and  self.average <= 100):
+                self.grade = "A"
+            elif(self.average > 80 and self.average < 90):
+                self.grade = "B"
+            elif (self.average > 70 and self.average < 80):
+                self.grade = "C"
+            else:
+                self.grade = "F"
+            return self.grade
+
+        self.grade = grade_calc(self)
+
+
+
+class final(Demographics,Grade):
+    def __init__(self,*args,**kwargs):
+        super(final,self).__init__(*args,**kwargs)
+
+
+        print("__________STUDENT TRANSCRIPT_____________")
+        print("_________________________________________")
+        print()
+        print("Student ID:              ",self.ID)
+        print("Student Age:             ",self.Age,)
+        print("Student Gender:          ",self.Gender)
+        print("In-state/Out-of-state:   ",self.In_state)
+
+        print()
+
+        print("Math Score:             ", self.Math_score)
+        print("Science Score:          ", self.Science_score)
+        print("English Score:          ", self.English_score)
+        print()
+        print(" Grade:                   ", self.grade)
+        print("Tution Fees:                     ", self.fees * 3)
+        print()
+
+        print("__________________________________________")
+        print("___________________________________________")
+
+def student_info():
+    ID = int(input("Plese enter your ID : "))
+    Age = int(input("Plese enter your Age : "))
+    Gender = str(input("Plese enter your Gender : "))
+    In_state = str(input("Plese enter your yes for in_state otherwise no : "))
+
+    Math_score = float(input("Please enter Math Score: "))
+    Science_score = float(input("Please enter Scince Score: "))
+    English_score = float(input("Please enter English Score: "))
+# validation for negative and over 100 scores
+    while((Math_score < 0 or Math_score > 100) or (Science_score < 0 or Science_score > 100) or (English_score < 0 or English_score > 100)):
+        print("Plese enter Right set of your scores 'NO NEGATIVES' & 'NO OVER 100'")
+        Math_score = float(input("Please enter Math Score: "))
+        Science_score = float(input("Please enter Scince Score: "))
+        English_score = float(input("Please enter English Score: "))
+#calling child class final which will print the transcript
+    transcript = final(ID,Age,Gender,In_state,Math_score,Science_score,English_score)
+
+student_info()
